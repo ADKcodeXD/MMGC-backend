3304eac3ca0c61d17c7a3a9b59d36e4475dbe4c9
ADK <88375547+ADKcodeXD@users.noreply.github.com>
2022-11-08 09:53:53 +0800

    add: types define

--- src/types/global.ts
+++ src/types/global.ts
@@ -17,5 +17,78 @@ declare global {
 		index: number
 		fn: Function
 	}
+
+	interface I18N {
+		/**
+		 * 中文名
+		 */
+		cn: string
+		/**
+		 * 英文名
+		 */
+		en?: null | string
+		/**
+		 * 日文名
+		 */
+		jp?: null | string
+	}
+
+	interface Staff {
+		/**
+		 * 评委id列表
+		 */
+		judges?: string[] | null
+		/**
+		 * 主办人用户id
+		 */
+		organizer?: null | string
+		/**
+		 * 翻译人员
+		 */
+		translator?: string[] | null
+	}
+
+	interface Sns {
+		bilibili?: string;
+		/**
+		 * niconico网站
+		 */
+		niconico?: string;
+		/**
+		 * 个人网站
+		 */
+		personalWebsite?: string;
+		/**
+		 * 推特网站
+		 */
+		twitter?: string;
+		/**
+		 * youtube频道
+		 */
+		youtube?: string;
+	}
+
+	interface DownloadLink {
+		baidu?: string;
+		google?: string;
+		onedrive?: string;
+		other?: string;
+	}
+
+	interface LoginVo {
+		/**
+		 * 是否收藏
+		 */
+		isCollect: boolean;
+		/**
+		 * 是否点赞
+		 */
+		isLike: boolean;
+		/**
+		 * 是否已投票
+		 */
+		isPoll: boolean;
+	}
+	
 }
-export {}
+export { }
