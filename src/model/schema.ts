2acbaee4774bf395dbfd83e8e31c90e23db06107
ADK <88375547+ADKcodeXD@users.noreply.github.com>
2022-11-08 10:47:34 +0800

    add: add activity types

--- src/model/schema.ts
+++ src/model/schema.ts
@@ -1,11 +1,3 @@
 import mongoose from 'mongoose'
 
-export const activity = new mongoose.Schema({
-	activityId: Number,
-	activityName: String,
-	createTime: Date,
-	sponsorId: String,
-	sponsorName: String,
-	days: Number,
-	movieNums: Number
-})
+export const activity = new mongoose.Schema(Activity.ActivityModel)
