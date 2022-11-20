71ace22d52b62eac63a1af8615eb3161e2821fb2
ADKCodeXD <790532173@qq.com>
2022-11-21 00:11:14 +0800

    fix: 🐛  optimize moment util

--- src/common/utils/moment.ts
+++ src/common/utils/moment.ts
@@ -1,5 +1,5 @@
 import moment from 'moment'
 
-export const formatTime = (time: string, format = 'YYYY-MM-DD HH:mm:ss') => {
+export const formatTime = (time: string | number | Date, format = 'YYYY-MM-DD HH:mm:ss') => {
 	return moment(time).format(format)
 }
