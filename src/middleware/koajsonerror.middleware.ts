60a03a053c84a8dd513659523102065e8f0ce91e
ADKCodeXD <790532173@qq.com>
2022-11-23 00:48:10 +0800

    fix: 🐛  support dev and prod switch

--- src/middleware/koajsonerror.middleware.ts
+++ src/middleware/koajsonerror.middleware.ts
@@ -2,9 +2,14 @@ import { Context } from 'koa'
 import KoaError from 'koa-json-error'
 import Result from '~/common/result'
 
+/**
+ * 处理抛出的异常和错误 假设在开发环境下全盘抛出 如果在生产环境下则动态判断是否为全中文提示，如是，则抛出message
+ */
 export const Error = KoaError({
 	format: (err: any) => {
-		return Result.fail(err.status, err.message, err.stack)
+		const reg = /^[\u4e00-\u9fa5][^%&',;=?$\x22]+$/
+		const message = process.env.NODE_ENV === 'dev' ? err.message : reg.test(err.message) ? err.message : '服务器内部错误！'
+		return Result.fail(err.status, message, process.env.NODE_ENV === 'dev' ? err.stack : null)
 	}
 })
 
