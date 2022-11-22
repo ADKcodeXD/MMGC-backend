f746ab47d51062c9d8f9e30a8eb2f86b3a3fbf78
ADKCodeXD <790532173@qq.com>
2022-11-23 00:48:55 +0800

    add: ⭐  login api

--- src/controller/member.controller.ts
+++ src/controller/member.controller.ts
@@ -1,13 +1,16 @@
 import { RESULT_CODE, RESULT_MSG } from '~/types/enum'
 import Result from '~/common/result'
 import { Body, Controller, PostMapping } from '~/common/decorator/decorator'
-import { copyProperties } from '~/common/utils'
+import { aesDecrypt, copyProperties } from '~/common/utils'
 import { Validtor } from '~/middleware/ajv.middleware'
 import { memberParamsValidate } from '~/common/validate/validate'
 import { MemberParams } from 'Member'
 import MemberService from '~/service/member.service'
 import { MemberParamsEntity } from '~/entity/member.entity'
 import { EmailUtil } from '~/common/utils/emailutil'
+import { createJsonWebToken } from '~/common/utils/jwtutil'
+import config from '~/config/config.default'
+import crypto from 'node:crypto'
 
 @Controller('/user')
 export default class MemberController {
@@ -33,10 +36,33 @@ export default class MemberController {
 		if (await email.verifyCode(registerParams.email, registerParams.verifyCode)) {
 			const params = new MemberParamsEntity()
 			copyProperties(registerParams, params)
-			const id = await this.memberService.save(params)
-			return Result.success(RESULT_CODE.SUCCESS, RESULT_MSG.SUCCESS, id)
+			const member = await this.memberService.save(params)
+			const token = createJsonWebToken(member, config.JWT_SECRET || 'jwt-token', 3600 * 48)
+			return Result.success(RESULT_CODE.SUCCESS, RESULT_MSG.SUCCESS, token)
 		} else {
 			return Result.fail(RESULT_CODE.VERIFY_ERROR, RESULT_MSG.VERIFY_ERROR, null)
+		}
+	}
+
+	@PostMapping('/login')
+	async login(@Body() loginParams: { username: string; password: string }) {
+		const user = await this.memberService.findMemberByUsername(loginParams.username)
+		if (!user) {
+			return Result.fail(RESULT_CODE.USER_NOTFOUND, RESULT_MSG.USER_NOTFOUND, null)
+		}
+		if (user) {
+			const key = crypto.scryptSync(config.AES_PASSWORD || '', config.AES_SALT || '', 16)
+			const decryptData = aesDecrypt(user.password, key)
+			if (decryptData === loginParams.password) {
+				const userVo = this.memberService.copyToVo(user)
+				return Result.success(
+					RESULT_CODE.SUCCESS,
+					RESULT_MSG.SUCCESS,
+					createJsonWebToken(userVo, config.JWT_SECRET || 'jwt-token', 3600 * 48)
+				)
+			} else {
+				return Result.fail(RESULT_CODE.USER_NOTFOUND, RESULT_MSG.USER_NOTFOUND, null)
+			}
 		}
 	}
 }
