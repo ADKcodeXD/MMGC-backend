344c917825a72fe02ade4ae396776abf93d74899
ADKCodeXD <790532173@qq.com>
2022-11-23 00:49:31 +0800

    feat: 🎉  auto incrementId

---
+++ src/service/increment.service.ts
@@ -0,0 +1,30 @@
+import { Singleton } from '~/common/decorator/decorator'
+import { Increment } from '~/model'
+
+@Singleton()
+export default class IncrementService {
+	static getInstance() {
+		console.log('dont have Singleton')
+		return new this()
+	}
+
+	increment = Increment
+
+	/**
+	 * 获取自增id
+	 * @param collection 集合的名字
+	 */
+	async incrementId(collection: string): Promise<number> {
+		const res = <IncrementType>await this.increment.findOne({ coll: 'members' })
+		let id = 0
+		if (!res) {
+			await new this.increment({ coll: collection, currentValue: 100 }).save()
+			id = 100
+		} else {
+			await this.increment.updateOne({ coll: res.coll }, { currentValue: res.currentValue + 1 })
+			id = res.currentValue
+		}
+
+		return id
+	}
+}
--- src/service/member.service.ts
+++ src/service/member.service.ts
@@ -2,16 +2,16 @@ import { MemberVo, MemberModel } from 'Member'
 import { Singleton } from '~/common/decorator/decorator'
 import { aesEncrypt, copyProperties } from '~/common/utils'
 import { MemberModelEntity, MemberParamsEntity, MemberVoEntity } from '~/entity/member.entity'
-import { Member, Increment } from '~/model'
+import { Member } from '~/model'
 import BaseService from './base.service'
 import crypto from 'node:crypto'
 import config from '~/config/config.default'
+import IncrementService from './increment.service'
 
 @Singleton()
 export default class MemberService extends BaseService {
 	memberModel = Member
-	increment = Increment
-
+	incrementService = IncrementService.getInstance()
 	static getInstance() {
 		console.log('dont have Singleton')
 		return new this()
@@ -38,6 +38,11 @@ export default class MemberService extends BaseService {
 		return memberModel
 	}
 
+	async findMemberByUsername(username: string) {
+		const memberModel = await this.memberModel.findOne({ username: username })
+		return memberModel
+	}
+
 	async findMemberVoByEmail(email: string) {
 		const reg = /^[a-zA-Z0-9_.]+@[a-zA-Z0-9-]+[.a-zA-Z]+$/
 		if (!reg.test(email)) {
@@ -56,25 +61,18 @@ export default class MemberService extends BaseService {
 	}
 
 	async save(memberParams: MemberParamsEntity) {
-		const res = <IncrementType>await this.increment.findOne({ coll: 'members' })
-		let id = 0
-		if (!res) {
-			new this.increment({ coll: 'members', currentValue: 100 }).save()
-			id = 100
-		} else {
-			this.increment.updateOne({ coll: 'members' }, { currentValue: res.currentValue + 1 })
-			id = res.currentValue
-		}
 		const model = new MemberModelEntity()
-		model.memberId = id
+		model.memberId = await this.incrementService.incrementId('members')
 		copyProperties(memberParams, model)
-		const key = crypto.scryptSync(config.AES_PASSWORD || '', config.AES_SALT || '', 32)
+
+		// 加密密码
+		const key = crypto.scryptSync(config.AES_PASSWORD || '', config.AES_SALT || '', 16)
 		const password = aesEncrypt(model.password, key)
 		model.password = password
-		console.log(model)
+
 		const memDoc = new this.memberModel(model)
 		const newMember = await memDoc.save()
-		return newMember.memberId
+		return this.copyToVo(newMember)
 	}
 
 	copyToVo(memberModel: MemberModel) {
