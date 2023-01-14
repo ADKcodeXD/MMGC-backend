c178d5944b9c575b873588044fb4fca3a764fe48
ADKCodeXD <790532173@qq.com>
2023-01-14 23:48:49 +0800

    feat: 🎉  auto update collections current increment ID
    这个修改是为了能够使increment数据库自动记录下最新的自定义的主键，如果不使用这个功能会产生自定义主键重复的后果

--- src/service/increment.service.ts
+++ src/service/increment.service.ts
@@ -1,3 +1,4 @@
+import { Model } from 'mongoose'
 import { Singleton } from '~/common/decorator/decorator'
 import { Increment } from '~/model'
 
@@ -13,18 +14,31 @@ export default class IncrementService {
 	/**
 	 * 获取自增id
 	 * @param collection 集合的名字
+	 * @param coll 假设需要防止自增id与数据库内的id冲突 需要设置此项
 	 */
-	async incrementId(collection: string): Promise<number> {
+	async incrementId(collection: string, coll?: { model: Model<any>; key: string }): Promise<number> {
 		const res = <IncrementType>await this.increment.findOne({ coll: collection })
 		let id = 0
 		if (!res) {
 			await new this.increment({ coll: collection, currentValue: 100 }).save()
 			id = 100
 		} else {
-			await this.increment.updateOne({ coll: res.coll }, { currentValue: res.currentValue + 1 })
 			id = res.currentValue
+			// 假设有输入详细的coll参数
+			if (coll) {
+				const hasResult = await coll.model.findOne({ [coll.key]: id })
+				if (hasResult) {
+					// 获取数据库中最新的数据 以这个数据id+1的为准获取新id
+					const newest = await coll.model
+						.find({})
+						.sort({ [coll.key]: -1 })
+						.limit(1)
+					id = newest[0][coll.key]
+				}
+			}
+			id = id + 1
+			await this.increment.updateOne({ coll: res.coll }, { currentValue: id })
 		}
-
 		return id
 	}
 }
