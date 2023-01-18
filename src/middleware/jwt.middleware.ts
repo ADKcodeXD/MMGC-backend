a90c5f11b246ab2b05202cf5fe913bcf4b03f768
ADKCodeXD <790532173@qq.com>
2023-01-18 19:42:20 +0800

    config: 🔧  add whitelist

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -1,10 +1,14 @@
 import config from '~/config/config.default'
 import Jwt from 'koa-jwt'
-import { verifyToken } from '~/common/utils/jwtutil'
 
-const whiteList = ['/email', '/user/register', '/user/login'].map(
-	item => new RegExp(`^${config.MMGC_PREFIX}${item}`.split('/').join('\\/'))
-)
+const whiteList = [
+	'/email',
+	'/user/register',
+	'/user/login',
+	'/config/getConfig',
+	'/activity/getActivityList',
+	'/activity/getActivityDetail'
+].map(item => new RegExp(`^${config.MMGC_PREFIX}${item}`.split('/').join('\\/')))
 
 export default Jwt({
 	secret: config.JWT_SECRET || 'jwt-secret'
