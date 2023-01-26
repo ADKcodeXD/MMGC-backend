610ef0116c611a525499fd1bfd5ed7718a2b4443
adkcodeMobilePC <790532173@qq.com>
2023-01-26 21:14:10 +0800

    fix: email code bug

--- src/middleware/jwt.middleware.ts
+++ src/middleware/jwt.middleware.ts
@@ -1,8 +1,7 @@
 import config from '~/config/config.default'
 import Jwt from 'koa-jwt'
-import { verifyToken } from '~/common/utils/jwtutil'
 
-const whiteList = ['/email', '/user/register', '/user/login'].map(
+const whiteList = ['/email', '/user/register', '/user/login', '/activity', 'config'].map(
 	item => new RegExp(`^${config.MMGC_PREFIX}${item}`.split('/').join('\\/'))
 )
 
