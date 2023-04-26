3fb02719e3d06d59909c1a99d7693737cbf9add8
ADKCodeXD <790532173@qq.com>
2023-04-26 13:43:25 +0800

    fix: bugs expect play time

--- src/service/movie.service.ts
+++ src/service/movie.service.ts
@@ -207,7 +207,7 @@ export default class MovieService extends BaseService {
 		}
 		if (movieModel.authorId) vo.author = await this.memberService.findMemberVoByMemberId(movieModel.authorId)
 		if (movieModel.expectPlayTime) {
-			const time = parseInt(formatTime(movieModel.expectPlayTime, 'x'))
+			const time = new Date(movieModel.expectPlayTime).getTime()
 			vo.expectPlayTime = formatTime(movieModel.expectPlayTime)
 			if (time > Date.now()) {
 				vo.isPublic = false
